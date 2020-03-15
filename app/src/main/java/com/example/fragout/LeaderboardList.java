dd28678a75ce3f7533b441eb74cef7950c69613e
PrithwishNag <prithwish.nag@gmail.com>
2020-03-15 11:04:41 +0530

    updated

---
+++ app/src/main/java/com/example/fragout/LeaderboardList.java
@@ -0,0 +1,14 @@
+package com.example.fragout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeaderboardList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.);
+    }
+}
